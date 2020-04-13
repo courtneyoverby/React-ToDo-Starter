45f5465e0e173b2bd3505d94d5fb0ce56dc3017a
Courtney Overby <brioverby@gmail.com>
2020-04-13 10:21:51 -0500

    TaskList.js App.js throwing error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+import TaskList from "co/TaskList/TaskList";
 
 class App extends Component {
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    axios
+      .get("/tasks")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.warn(err));
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Tasks</h1>
+        <TaskList />
       </div>
     );
   }
---
+++ src/components/App/TaskList/TaskList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TaskList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+      </div>
+    );
+  }
+}
+
+export default TaskList;
